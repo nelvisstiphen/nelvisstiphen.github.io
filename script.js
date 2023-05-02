6f8f98bec7964387852ee4c22ac31358e19a5217
nelvisstiphen <101031865+nelvisstiphen@users.noreply.github.com>
2023-05-02 17:14:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
     $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/nelvissalazar.16@gmail.com", {
+    fetch("https://formsubmit.co/ajax/nelvissalazar.31@gmail.com", {
         method: "POST",
         body: new FormData(e.target),
     })
